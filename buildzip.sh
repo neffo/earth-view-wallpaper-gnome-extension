7d027a595a7e86d2ca1905c002dcd73981bc602e
Michael Carroll <ineffable@gmail.com>
2021-10-19 22:00:32 +1000

    improve build and install scripts

--- buildzip.sh
+++ buildzip.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-npm test
+EXTENSION_NAME=GoogleEarthWallpaper@neffo.github.com
+ZIP_NAME=$EXTENSION_NAME.zip
+
+# stop build if this doesn't work
+npm --version && (npm test; if [ $? -ne 0 ]; then exit 1; fi)
 
 glib-compile-schemas schemas/
-intltool-extract --type=gettext/glade Settings.ui 
-intltool-extract --type=gettext/glade Settings4.ui
-xgettext -k -k_ -kN_ -o locale/GoogleEarthWallpaper.pot Settings.ui.h Settings4.ui.h extension.js prefs.js utils.js --from-code=UTF-8
+intltool-extract --type=gettext/glade ui/Settings.ui 
+intltool-extract --type=gettext/glade ui/Settings4.ui
+xgettext -k -k_ -kN_ -o locale/GoogleEarthWallpaper.pot ui/Settings.ui.h ui/Settings4.ui.h extension.js prefs.js utils.js --from-code=UTF-8
 
 for D in locale/*; do
     if [ -d "${D}" ]; then
@@ -13,8 +17,8 @@ for D in locale/*; do
     fi
 done
 
-rm GoogleEarthWallpaper@neffo.github.com.zip
+rm $ZIP_NAME
 
-zip -r GoogleEarthWallpaper@neffo.github.com.zip *
+zip -r $ZIP_NAME *
 
-zip -d GoogleEarthWallpaper@neffo.github.com.zip screenshot/* screenshot buildzip.sh Settings.ui.h *.py *~ *.sh .* translations.txt *.h package.json *.yaml *.po *.pot
+zip -d $ZIP_NAME screenshot/* screenshot buildzip.sh Settings.ui.h *.py *~ *.sh .* translations.txt *.h package.json *.yaml *.po *.pot
--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ INSTALL_PATH=~/.local/share/gnome-shell/extensions
 ZIP_NAME=$EXTENSION_NAME.zip
 
 ./buildzip.sh
+if [ $? -ne 0 ]; then 
+    echo "ERROR: build failed!"
+    exit 1
+fi
 
 # this might be better to use the official installer script if it handles .zip files
 unzip -o $ZIP_NAME -d $INSTALL_PATH/$EXTENSION_NAME/
