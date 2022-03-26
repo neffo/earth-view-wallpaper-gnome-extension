13ff5dfc43794d3055db8d8ae4e06894efbf47a0
Michael Carroll <ineffable@gmail.com>
2022-03-26 20:28:21 +1000

    add support for GNOME 42, fix license link

--- ui/Settings.ui.h
+++ ui/Settings.ui.h
@@ -17,7 +17,7 @@ char *s = N_("Background");
 char *s = N_("Changes since last version");
 char *s = N_("Change log");
 char *s = N_("<span size=\"small\">This program comes with ABSOLUTELY NO WARRANTY.\n"
-             "See the <a href=\"https://www.gnu.org/licenses/old-licenses/gpl-2.0.html\">GNU General Public License, version 3 or later</a> for details.</span>");
+             "See the <a href=\"https://www.gnu.org/licenses/gpl-3.0.html\">GNU General Public License, version 3 or later</a> for details.</span>");
 char *s = N_("https://github.com/neffo/earth-view-wallpaper-gnome-extension");
 char *s = N_("Based on NASA APOD Wallpaper extension by Elia Argentieri");
 char *s = N_("This GNOME shell extension sets your wallpaper to a random Google Earth photo from a selection of curated locations.");
